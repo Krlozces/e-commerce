69500d8e1b4dd67f08d3d0e1ef56161e3bfe0a52
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-22 22:37:39 -0500

    Add to cart btn added.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
-
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Card({  title, description, precio, image }) {
     return (
         <>
@@ -6,12 +7,16 @@ export default function Card({  title, description, precio, image }) {
                 <img className="w-full" src={image} alt={title} />
                 <div className="px-2 py-4 relative bg-white">
                     <div className="mb-2"> {title} </div>
-                        <p className="text-gray-700 text-base">
-                            {description}
-                        </p>
+                    <p className="text-gray-700 text-base">
+                        {description}
+                    </p>
+                    <div className='flex justify-end'>
+                        <button className="px-6 py-2 bg-slate-200 rounded hover:bg-slate-400" type="button">Add to cart</button>
+                    </div>
                 </div>
                 <span className="absolute top-52 mx-2 my-4 py-2 px-4 bg-slate-200 rounded"> S/.{precio} </span>
-                <button className="absolute"> image </button>
+                
+                <a className="absolute top-4 right-6 m-2"> <FontAwesomeIcon icon={faHeart} /> </a>
             </div>
         </>
     )
