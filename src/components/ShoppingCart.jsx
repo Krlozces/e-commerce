34b2133e0d026c7c002dba0ca798ad0f1081eb69
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-25 01:05:46 -0500

    Display flex added.

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { DataContext } from "./DataContext";
-import images from '../../public/images.json';
 import Card from "./Card";
 export default function ShoppingCart() {
     const { cart, setCart } = useContext(DataContext);
-    const { items, setItems } = useContext(DataContext);
-    const [cartTotal, setCartTotal] = useState(0);
+    const { cartTotal, setCartTotal, removeFromCart } = useContext(DataContext);
     
     useEffect(() => {
         total();
@@ -14,34 +12,36 @@ export default function ShoppingCart() {
     const total = () => {
         let totalVal = 0;
         for(let i = 0; i < cart.length; i++){
-            totalVal += cart[i].price;
+            totalVal += parseFloat(cart[i].price);
         }
         setCartTotal(totalVal);
     };
 
-    const removeFromCart = (el) => {
-        let hardCopy = [...cart];
-        hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-        setCart(hardCopy);
-    }
+    const handleRemoveFromCart = (product) => {
+        removeFromCart(product);
+    };
+
     return (
         <>
             <h2 className="font-bold text-2xl text-center my-4">Carrito de compras</h2>
-            {cart && cart.length > 0 ? (
-                cart.map((cartItem) => (
-                    <div key={cartItem.id} className="cart-item">
-                        <Card
-                            title={cartItem.title}
-                            description={cartItem.description}
-                            precio={cartItem.price}
-                            image={cartItem.image}
-                            onClick={() => removeFromCart(cartItem)}
-                        />
-                    </div>
-                ))
-            ) : (
-                <p className="text-center">Tu carrito está vacío</p>
-            )}
+            <div className="flex justify-between">
+                {cart && cart.length > 0 ? (
+                    cart.map((cartItem) => (
+                        <div key={cartItem.id}>
+                            <Card
+                                title={cartItem.title}
+                                description={cartItem.description}
+                                precio={cartItem.price}
+                                image={cartItem.image}
+                                handlerOnClick={() => handleRemoveFromCart(cartItem)}
+                                val={"Remove from Cart"}
+                            />
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-center">Tu carrito está vacío</p>
+                )}
+            </div>
         </>
     )
 }
