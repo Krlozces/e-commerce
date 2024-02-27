736e9c6c9bfb27c205c34be00daf9ad0606be603
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-27 00:21:34 -0500

    Breakpoints added.

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -17,7 +17,33 @@ export default function CardSection() {
         slidesToShow: 4,
         slidesToScroll: 1,
         nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
+        prevArrow: <SamplePrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     return (
