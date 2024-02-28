0516d30c16b0a22ee647aa86377dd19275201ed2
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-28 11:53:44 -0500

    Responsive card sm

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({  title, description, precio, image, handlerOnClic
     
     return (
         <>
-            <div className="max-w-sm relative overflow-hidden bg-gray-400 mx-2">
+            <div className="max-w-sm relative overflow-hidden bg-gray-400 lg:mx-2 mx-auto">
                 <img className="w-full" src={image} alt={title} />
                 <div className="px-2 py-4 relative bg-white">
                     <div className="mb-2"> {title} </div>
@@ -15,7 +15,7 @@ export default function Card({  title, description, precio, image, handlerOnClic
                         <button className="px-6 py-2 bg-slate-200 rounded hover:bg-slate-400" type="button" onClick={handlerOnClick}>{val}</button>
                     </div>
                 </div>
-                <span className="absolute top-52 mx-2 my-4 py-2 px-4 bg-slate-200 rounded"> S/.{precio} </span>
+                <span className="absolute lg:top-52 top-80 mx-2 my-4 py-2 px-4 bg-slate-200 rounded"> S/.{precio} </span>
                 
                 <a className="absolute top-4 right-6 m-2"> <FontAwesomeIcon icon={faHeart} /> </a>
             </div>
