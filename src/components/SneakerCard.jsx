b4b38d9c1708659f6282720427d5e5cdf6609d1b
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-28 12:02:00 -0500

    Responsive slider

--- src/components/SneakerCard.jsx
+++ src/components/SneakerCard.jsx
@@ -19,6 +19,32 @@ export default function SneakerCard() {
         autoplaySpeed: 3000,
         cssEase: "linear",
         lazyLoad: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     const items = [
