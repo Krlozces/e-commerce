76f7ba5954cdfd904157387a8641e688025c4f9f
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-02-28 17:24:25 -0500

    Responsive component.

--- src/components/ImageCollection.jsx
+++ src/components/ImageCollection.jsx
@@ -6,12 +6,13 @@ import Sneaker04 from '../assets/images/Sneaker04.jpg';
 export default function ImageCollection() {
     return (
         <>
-            <div className="grid grid-cols-1 lg:grid-cols-8 relative h-96 w-full my-4">
-                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker02})` }}></div>
-                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker02})` }}></div>
+            <div className="grid grid-cols-1 lg:grid-cols-8 lg:grid-rows-4 relative h-96 w-full my-4">
+                <div className="col-span-1 lg:col-span-2 row-span-2 lg:row-span-4 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker02})` }}></div>
+                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-span-4 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker02})` }}></div>
                 <div className="row-span-2 col-span-1 lg:col-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker03})` }}></div>
-                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-start-2 lg:col-start-5 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker04})` }}></div>
-                <div className="row-span-2 lg:col-span-2 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker})` }}></div>
+                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-start-3 lg:col-start-5 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker04})` }}></div>
+                <div className="row-span-2 col-span-1 lg:col-span-2 lg:row-start-3 lg:col-start-7 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker04})` }}></div>
+                <div className="row-span-2 col-span-1 lg:col-start-5 lg:row-start-1 lg:col-span-2 lg:row-span-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${Sneaker})` }}></div>
                 <a href="#" className="w-[250px] hover:bg-gray-200 absolute top-64 right-0 left-0 mx-auto lg:left-1/2 z-10 text-center bg-white rounded py-2 px-4 font-semibold">DESCUBRE MÁS</a>
             </div>
             
