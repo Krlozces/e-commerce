451e21300731fe560a92ef7b8b7e0941fb2c1ff1
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-03-11 23:58:44 -0500

    ReactDOM added to render the view in Laravel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './views/Home.jsx';
 
+ReactDOM.createRoot(document.getElementById('react-app')).render(<Home />);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
