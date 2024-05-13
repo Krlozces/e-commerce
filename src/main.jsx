615930968b90aa5ad65db65ebd7da97f50d7ea8a
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-12 20:25:25 -0500

    Route using Laravel commented

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import Home from './views/Home.jsx';
 
-ReactDOM.createRoot(document.getElementById('react-app')).render(<Home />);
+// ReactDOM.createRoot(document.getElementById('react-app')).render(<Home />);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
