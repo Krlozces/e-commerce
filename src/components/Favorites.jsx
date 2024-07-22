5f18a0c6d78f8030b0f329220935dc5ab19bd505
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-22 00:10:35 -0500

    Some text added

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,8 +1,10 @@
+import { NavLink } from "react-router-dom";
 
 export default function Favorites() {
     return (
         <>
             <h2 className="text-black">Favoritos</h2>
+            <span>¿Quieres ver tus favoritos? <NavLink to="#">Únete</NavLink> <NavLink to="/login">Inicia Sesión</NavLink></span>
         </>
     )
 }
